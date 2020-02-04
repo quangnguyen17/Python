783fa8a73d0212715a36e96a6fd7b46fdbb06fca
Quang Nguyen <wan15112001@gmail.com>
2020-02-04 11:11:28 -0800

    algorithms

--- python/fundamentals/daily_algorithms.py
+++ python/fundamentals/daily_algorithms.py
@@ -1,6 +1,5 @@
 
-# Parens Valid
-# Create a function that, given an input string str , returns a boolean whether parentheses in str are valid. Valid sets of parentheses always open before they close.
+# 1. Parens Valid: Create a function that, given an input string str , returns a boolean whether parentheses in str are valid. Valid sets of parentheses always open before they close.
 
 
 def paren_valid(str):
@@ -23,8 +22,8 @@ print(paren_valid("N(0(p)3"))
 print(paren_valid("N(0)t ) 0( k"))
 print(paren_valid("a(b))(c"))
 
-# Braces Valid
-# Given a sequence of parentheses, braces and brackets, determine whether it is valid.
+
+# 2. Braces Valid: Given a sequence of parentheses, braces and brackets, determine whether it is valid.
 
 
 def braces_valid(string):
