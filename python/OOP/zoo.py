ad7afd5bd6dd9594191c972e8a13f1cc1e06ae67
Quang Nguyen <wan15112001@gmail.com>
2020-02-03 16:14:05 -0800

    added zoo.py

--- python/OOP/zoo.py
+++ python/OOP/zoo.py
@@ -2,6 +2,23 @@
 # Assignment: Zoo
 
 
+class Zoo:
+    def __init__(self, zoo_name):
+        self.animals = []
+        self.name = zoo_name
+
+    def add_lion(self, name):
+        self.animals.append(Lion(name))
+
+    def add_tiger(self, name):
+        self.animals.append(Tiger(name))
+
+    def print_all_info(self):
+        print("-"*30, self.name, "-"*30)
+        for animal in self.animals:
+            animal.display_info()
+
+
 class Animal:
     def __init__(self, name, age, health_level, happiness_level):
         self.name = name
@@ -15,20 +32,43 @@ class Animal:
 
     def display_info(self):
         print("Name: " + self.name)
-        print("Age: " + self.age)
-        print("Happiness Level: " + self.happiness_level)
+        print("Age: " + str(self.age))
+        print("Happiness Level: " + str(self.happiness_level))
 
 
 class Tiger(Animal):
-    def __init__(self):
-        super()
+    def __init__(self, name):
+        super().__init__(name, 0, 0, 0)
+        self.habitats = ["Open grasslands", "Savannas", "Rainforests", "..."]
+
+    def display_info(self):
+        super().display_info()
+        print("Habitats: " + str(self.habitats))
 
 
-class Tiger(Animal):
-    def __init__(self):
-        super()
+class Lion(Animal):
+    def __init__(self, name):
+        super().__init__(name, 0, 0, 0)
+        self.habitats = ["Open Grasslands", "Savanna", "Shrubland", "..."]
+
+    def display_info(self):
+        super().display_info()
+        print("Habitats: " + str(self.habitats))
 
 
-class Tiger(Animal):
-    def __init__(self):
-        super()
+class Bear(Animal):
+    def __init__(self, name):
+        super().__init__(name, 0, 0, 0)
+        self.habitats = ["Deserts", "Forests", "..."]
+
+    def display_info(self):
+        super().display_info()
+        print("Habitats: " + str(self.habitats))
+
+
+zoo1 = Zoo("John's Zoo")
+zoo1.add_lion("Nala")
+zoo1.add_lion("Simba")
+zoo1.add_tiger("Rajah")
+zoo1.add_tiger("Shere Khan")
+zoo1.print_all_info()
