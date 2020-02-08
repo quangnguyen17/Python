5f4fc8a002f8e678f431740cd8dfdd2e786f3b98
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-07 22:18:34 -0800

    finished login and registration

---
+++ django/django_full_stack/login_and_registration/login_n_registration_proj/login_n_registration_app/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .models import User
+import bcrypt
+# Create your views here.
+
+
+def index(request):
+    if request.session['user_id'] != None:
+        uid = request.session['user_id']
+        return redirect(f"/success/{uid}")
+    else:
+        return render(request, 'index.html')
+
+
+def log_in(request):
+    errors = User.objects.login_validator(request.POST)
+
+    if len(errors) > 0:
+        return render(request, 'index.html', {'messages': errors.values()})
+    else:
+        for user in User.objects.all():
+            password_matched = bcrypt.checkpw(
+                request.POST['password'].encode(), user.password.encode())
+            email_matched = user.email == request.POST['email']
+            if password_matched and email_matched:
+                request.session['user_id'] = user.id
+                return redirect("/")
+
+        return redirect("/", {'messages': ['Passwords and Emails do not match.']})
+
+
+def log_out(request):
+    request.session['user_id'] = None
+    return redirect("/")
+
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+
+    if len(errors) > 0:
+        return render(request, 'index.html', {'messages': errors.values()})
+    else:
+        password = bcrypt.hashpw(
+            request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        user = User.objects.create(
+            first_name=request.POST['first_name'], last_name=request.POST['last_name'], birthdate=request.POST['birthdate'], email=request.POST['email'], password=password)
+        request.session['user_id'] = user.id
+        return redirect(f"/success/{user.id}")
+
+
+def success(request, user_id):
+    if request.session['user_id'] == user_id:
+        return render(request, 'success.html', {'user': User.objects.all().get(id=user_id)})
+    else:
+        return redirect("/")
